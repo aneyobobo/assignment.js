53da68d616b3a3eafcd9fe543668758b2196a5c1
rosh <roshbon@gmail.com>
2023-02-20 10:24:42 +0100

    question1 solution

---
+++ assignment_20_02_23.js
@@ -0,0 +1,13 @@
+// 1. Return the Sum of Two Numbers. 
+// Write a function that takes two numbers as 
+// arguments and returns their sum.
+// samples below
+// addition(3, 2) ➞ 5
+// addition(-3, -6) ➞ -9
+// addition(7, 3) ➞ 10
+
+function sumOfTwoNumbers(firstNumber, secondNumber) {
+    let sum = firstNumber + secondNumber
+
+    return sum
+}
