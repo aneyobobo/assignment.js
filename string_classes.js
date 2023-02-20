e5b1e4abe3889cc76408e86bb2b00428f5de796d
rosh <roshbon@gmail.com>
2023-02-20 13:16:41 +0100

    uppercase and lowercase

---
+++ string_classes.js
@@ -0,0 +1,14 @@
+function callMynameToUpper(name) {
+    let newName = name.toUpperCase()
+
+    return newName
+}
+
+console.log(callMynameToUpper("olATunJi yusuF AjOa") )
+
+
+function callMynameToSmall(name) {
+    let newName = name.toLowerCase()
+
+    return newName
+}
